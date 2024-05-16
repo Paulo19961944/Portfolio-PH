b8ed10722139f0b8be5566002d89ffa9ebc9492a
Paulo Henrique <paulo24041996@gmail.com>
2024-05-16 11:14:37 -0300

    [fix] Ajustando Bugs no Javascript

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,13 +1,16 @@
+const bodyElement = window.document.body
 abrirMenuMobile.addEventListener('click', abrirMenu)
 fecharMenuMobile.addEventListener('click', fecharMenu)
 
 function abrirMenu(){
+    bodyElement.style.overflow = 'hidden'
     abrirMenuMobile.classList.add('abrirMenuResponsivo')  
     fecharMenuMobile.classList.add('fecharMenuResponsivo')
     menu.classList.add('menuResponsivo')
 }
 
 function fecharMenu(){
+    bodyElement.style.overflow = 'auto'
     abrirMenuMobile.classList.remove('abrirMenuResponsivo')  
     fecharMenuMobile.classList.remove('fecharMenuResponsivo')
     menu.classList.remove('menuResponsivo')
