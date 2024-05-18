3fb19e92660702079a3631c04d2852d1bdffe0e6
Paulo Henrique <paulo24041996@gmail.com>
2024-05-18 20:46:57 -0300

    [feature] Adicionando Comentários e Finalizando Projeto

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,17 +1,19 @@
-const bodyElement = window.document.body
-abrirMenuMobile.addEventListener('click', abrirMenu)
-fecharMenuMobile.addEventListener('click', fecharMenu)
+const bodyElement = window.document.body // Captura o Body
+abrirMenuMobile.addEventListener('click', abrirMenu) // Adiciona Evento Botão ao Evento de Abrir Menu
+fecharMenuMobile.addEventListener('click', fecharMenu) // Adiciona Evento ao Botão de Fechar Menu
 
+// Funçao para abrir o menu
 function abrirMenu(){
-    bodyElement.style.overflow = 'hidden'
-    abrirMenuMobile.classList.add('abrirMenuResponsivo')  
-    fecharMenuMobile.classList.add('fecharMenuResponsivo')
-    menu.classList.add('menuResponsivo')
+    bodyElement.style.overflow = 'hidden' // Trava a Tela
+    abrirMenuMobile.classList.add('abrirMenuResponsivo') // Adiciona a classe abrirMenuResponsivo
+    fecharMenuMobile.classList.add('fecharMenuResponsivo') // Adiciona a classe fecharMenuResponsivo
+    menu.classList.add('menuResponsivo') // Adiciona a Classe menuResponsivo
 }
 
+// Função para fechar o menu
 function fecharMenu(){
-    bodyElement.style.overflow = 'auto'
-    abrirMenuMobile.classList.remove('abrirMenuResponsivo')  
-    fecharMenuMobile.classList.remove('fecharMenuResponsivo')
-    menu.classList.remove('menuResponsivo')
+    bodyElement.style.overflow = 'auto' // Reseta no padrão original
+    abrirMenuMobile.classList.remove('abrirMenuResponsivo') // Remove a classe abrirMenuResponsivo
+    fecharMenuMobile.classList.remove('fecharMenuResponsivo') // Remove a classe fecharMenuResponsivo
+    menu.classList.remove('menuResponsivo') // Remove a classe menuResponsivo
 }
